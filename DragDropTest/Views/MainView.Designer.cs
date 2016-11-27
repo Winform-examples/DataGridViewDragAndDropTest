613f16d8221da4571b5e13ac26466c115921d181
Marc Hartsuiker <marc.hartsuiker@gmail.com>
2016-11-27 20:42:07 +0100

    Initial version

---
+++ DragDropTest/Views/MainView.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace DragDropTest
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id2Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.text2Column = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.idColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(675, 549);
+            this.splitContainer1.SplitterDistance = 279;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(675, 279);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Grid 1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id2Column,
+            this.text2Column});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(669, 260);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id2Column
+            // 
+            this.id2Column.HeaderText = "ID";
+            this.id2Column.MinimumWidth = 40;
+            this.id2Column.Name = "id2Column";
+            this.id2Column.ReadOnly = true;
+            this.id2Column.Width = 40;
+            // 
+            // text2Column
+            // 
+            this.text2Column.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.text2Column.HeaderText = "Text";
+            this.text2Column.Name = "text2Column";
+            this.text2Column.ReadOnly = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(675, 266);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Grid 2";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowDrop = true;
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idColumn,
+            this.textColumn});
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(669, 247);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // idColumn
+            // 
+            this.idColumn.HeaderText = "ID";
+            this.idColumn.MinimumWidth = 40;
+            this.idColumn.Name = "idColumn";
+            this.idColumn.ReadOnly = true;
+            this.idColumn.Width = 40;
+            // 
+            // textColumn
+            // 
+            this.textColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.textColumn.HeaderText = "Text";
+            this.textColumn.Name = "textColumn";
+            this.textColumn.ReadOnly = true;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 549);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainView";
+            this.Text = "Drop and drop test application";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id2Column;
+        private System.Windows.Forms.DataGridViewTextBoxColumn text2Column;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn textColumn;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ DragDropTest/Views/MainView.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DragDropTest
+{
+    public partial class MainView : Form
+    {
+        public MainView()
+        {
+            InitializeComponent();
+
+            SetupDataModels();
+            SetupGridViews();
+
+            dataGridView1.MouseDown += (s, e) => dataGridView1_OnMouseDown(s, e);
+            dataGridView1.MouseMove += (s, e) => dataGridView1_OnMouseMove(s, e);
+            dataGridView2.DragEnter += (s, e) => DataGridView2_OnDragEnter(s, e);
+            dataGridView2.DragDrop += (s, e) => DataGridVie2_OnDragDrop(s, e);
+
+        }
+
+        /// <summary>
+        /// Handles the Mouse down event for a control
+        /// </summary>
+        /// <param name="sender">Control from which the event was fired</param>
+        /// <param name="e">MouseEventArgs</param>
+        private void dataGridView1_OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dataGridView1.DoDragDrop(dataGridView1.SelectedRows, DragDropEffects.Copy);
+            }
+        }
+
+        /// <summary>
+        /// Handles the Mouse move event for a control
+        /// </summary>
+        /// <param name="sender">Control from which the event was fired</param>
+        /// <param name="e">MouseEventArgs</param>
+        private void dataGridView1_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Handles the Drag enter event for a control
+        /// </summary>
+        /// <param name="sender">Control from which the event was fired</param>
+        /// <param name="e">MouseEventArgs</param>
+        private void DataGridView2_OnDragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        /// <summary>
+        /// Handles the Drag and drop event for a control
+        /// </summary>
+        /// <param name="sender">Control from which the event was fired</param>
+        /// <param name="e">MouseEventArgs</param>
+        private void DataGridVie2_OnDragDrop(object sender, DragEventArgs e)
+        {
+            var rowData = e.Data.GetData(typeof(DataGridViewSelectedRowCollection));
+            if (rowData != null)
+            {
+                if (_dataModel2 == null)
+                {
+                    _dataModel2 = new List<DataModel>();
+                }
+
+                var rows = rowData as DataGridViewSelectedRowCollection;
+                foreach(var row in rows)
+                {
+                    var data = (row as DataGridViewRow).DataBoundItem as DataModel;
+                    if (data != null)
+                    {                        
+                        if (!_dataModel2.Contains(data))
+                        {
+                            _dataModel2.Add(data);
+                        }
+                    }
+                }
+
+                if (_dataModel2.Count > 0)
+                {
+                    var bindingSource = new BindingSource();
+                    bindingSource.DataSource = _dataModel2;
+                    dataGridView2.DataSource = bindingSource;
+                    bindingSource.ResetBindings(false);
+                }
+            }
+        }
+
+        private void SetupDataModels()
+        {
+            _dataModel1 = new List<DataModel>
+            {
+                new DataModel
+                {
+                    ID = 1,
+                    Text = "Test text 1",
+                },
+                new DataModel
+                {
+                    ID = 2,
+                    Text = "Test text 2",
+                },
+            };
+
+            _dataModel2 = new List<DataModel>();
+        }
+
+        private void SetupGridViews()
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.DataSource = _dataModel1;
+            dataGridView1.Columns["ID"].DataPropertyName = "ID";
+            dataGridView1.Columns["Text"].DataPropertyName = "Text";
+            dataGridView1.Columns["Text"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView1.RowHeadersVisible = false;
+
+            dataGridView2.Columns.Clear();
+            dataGridView2.DataSource = _dataModel2;
+            dataGridView2.Columns["ID"].DataPropertyName = "ID";
+            dataGridView2.Columns["Text"].DataPropertyName = "Text";
+            dataGridView2.Columns["Text"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dataGridView2.RowHeadersVisible = false;
+        }
+
+        private List<DataModel> _dataModel1;
+        private List<DataModel> _dataModel2;
+        //private Rectangle _mouseDragBox;
+    }
+}
